c2839ea22f157942bee6fcd33225e36895ae3150
Gillesedgy <edgygilles@pursuit.org>
2022-09-18 16:36:39 -0400

    added some stuff

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const h4 = document.querySelector("h4");
 const option = document.querySelector(".option");
 const ul = document.querySelector("ul");
 const input = document.querySelector("#shoutout");
-const info = document.querySelector("#info")
-const error = document.getElementById("err")
+const info = document.querySelector("#info");
+const error = document.getElementById("err");
 const Based_URL = "https://ghibliapi.herokuapp.com/people";
 let pplData; // saved resJson
 
@@ -13,65 +13,62 @@ fetch(Based_URL)
   .then((response) => response.json())
   .then((resJson) => {
     pplData = resJson;
- console.log(pplData)
+    //  console.log(pplData)
     resJson.forEach((person) => {
-    const name = person.name;
-    let option = document.createElement("option");
-    option.innerHTML = name;
-    option.value = name;
-    select.append(option);
-  })
+      const name = person.name;
+      let option = document.createElement("option");
+      option.innerHTML = name;
+      option.value = name;
+      select.append(option);
+    });
   })
   .catch((err) => console.log(err));
 
 //!
 select.addEventListener("change", () => {
-    console.log(pplData)
-const personData = pplData.find((el)=> el.name === select.value)
-console.log(personData) 
-info.innerHTML = ""; //section above the SHOUTOUT, below the submit
+  // console.log(pplData)
+
+  const personData = pplData.find((el) => el.name === select.value);
+  // console.log(personData)
+  info.innerHTML = ""; //section above the SHOUTOUT, below the submit
   const h4 = document.createElement("h4");
-    h4.textContent =personData.name;
+  h4.textContent = personData.name;
 
   let age = document.createElement("p");
-    age.innerHTML = `<b>Age:</b> ${personData.age}`;
+  age.innerHTML = `<b>Age:</b> ${personData.age}`;
 
   let eyeColor = document.createElement("p");
-    eyeColor.innerHTML = `<b>Eye Color:</b> ${personData.eye_color}`;
+  eyeColor.innerHTML = `<b>Eye Color:</b> ${personData.eye_color}`;
 
   let hairColor = document.createElement("p");
-    hairColor.innerHTML = `<b>Hair Color:</b> ${personData.hair_color}`;
-    info.append(h4, age, eyeColor, hairColor);
-
-})
+  hairColor.innerHTML = `<b>Hair Color:</b> ${personData.hair_color}`;
+  info.append(h4, age, eyeColor, hairColor);
+});
 //!Form
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  error.innerHTML=""
+  error.innerHTML = "";
   //! Error Message
   //? shout w/o person
-  if(!select.value ){
-     const err =  document.createElement('p')
-     err.innerText= "Please Select a name"
-     error.append(err) 
+  if (!select.value) {
+    const err = document.createElement("p");
+    err.innerText = "Please Select a name";
+    error.append(err);
+  } else if (!input.value) {
+    const err2 = document.createElement("p");
+    err2.innerHTML = `Please add a message for ${select.value}`;
+    error.append(err2);
+    //* to remove err message when typing for shoutout --> can create err out
+    input.addEventListener("input", () => {
+      err2.innerHTML = "";
+    });
+  } else {
+    const li = document.createElement("li");
+    const shoutOut = input.value;
+    li.innerHTML = `<strong>${select.value}:</strong> ${shoutOut}`;
+    ul.append(li);
   }
-  else if(!input.value){
-    const err2 =  document.createElement('p')
- err2.innerHTML =`Please add a message for ${select.value}` 
- error.append(err2)
- //* to remove err message when typing for shoutout --> can create err out
-input.addEventListener("input", ()=>{
-    err2.innerHTML=""
-})
-  }
-  else{
-      const li = document.createElement("li");
-      const shoutOut = input.value;
-      li.innerHTML = `<strong>${select.value}:</strong> ${shoutOut}`;
-      ul.append(li);
-  }
-//? person no shout
-form.reset();
+  form.reset();
 });
 //! RESET Button -> Shoutout
 const reset = document.createElement("button");
@@ -83,5 +80,5 @@ reset.style.color = "white";
 reset.style.textDecoration = "underline";
 ul.append(reset);
 reset.addEventListener("click", () => {
-  ul.innerHTML= ""
+  ul.innerHTML = "";
 });
